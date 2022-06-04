0edb1f01a3283fcfd4a0003abdbaa449d9f3af8d
Fernando <fsazevedo09@gmail.com>
2022-06-03 21:05:35 -0300

    Create bd for localStorage tests

--- src/js/taskStorage.js
+++ src/js/taskStorage.js
@@ -1,11 +1,13 @@
-
-
+// Função para gerar ID via timestamp
 function idGenerator() {
     var timestamp = new Date()
     var id = timestamp.getTime().toString()
     return id
 }
 
-
+let bdLocalStorage = [
+    {'task_txt': 'Estudar JS', 'status': ''},
+    {'task_txt': 'Estudar CSS', 'status': '-done'}
+]
 
 
