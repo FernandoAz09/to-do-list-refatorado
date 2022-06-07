7d34ff428275486bcd9f20584dbcee841fea8557
Fernando <fsazevedo09@gmail.com>
2022-06-06 22:00:01 -0300

    Update Input Validate

--- src/js/task.js
+++ src/js/task.js
@@ -1,105 +1,115 @@
-// // Enxergando os elementos do HTML
+// Enxergando os elementos do HTML
 
-// window.addEventListener('load', () => {
-//     const form = document.querySelector('#new-task-form')
-//     const input = document.querySelector('#new-task-input')
-//     const list_el = document.querySelector('#tasks')
+window.addEventListener('load', () => {
+    const form = document.querySelector('#new-task-form')
+    const input = document.querySelector('#new-task-input')
+    const list_el = document.querySelector('#tasks')
 
-//     // Iniciando os eventos do CRUD
-//     form.addEventListener('submit', (e) => {
-//         e.preventDefault()
+    const validateInput = () => input.value.trim().length > 0;
 
-//         const task = input.value
+    // Iniciando os eventos do CRUD
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
 
-//         if (!task) {
-//             alert('Please fill out the task')
-//             return
-//         }
+        const task = input.value
+        const inputIsValid = validateInput();
 
-//         // ------------------- CREATE/READ -------------------
-//         const task_el = document.createElement('div')
-//         task_el.classList.add('task')
+        if (!task) {
+            alert('Please fill out the task')
+            return
+        }
+
+        if (!inputIsValid) {
+            alert('Please WRITE out the task')
+            input.value = ''
+            return 
+        }
         
 
-//         const task_content_el = document.createElement('div')
-//         task_content_el.classList.add('content')
-
-//         task_el.appendChild(task_content_el)
-
-//         const task_input_el = document.createElement('input')
-//         task_input_el.classList.add('text')
-//         task_input_el.type = 'text'
-//         task_input_el.value = task
-//         task_input_el.setAttribute('readonly', 'readonly')
-//         task_input_el.setAttribute('id', idGenerator())
-
-//         task_content_el.appendChild(task_input_el)
-
-//         const task_actions_el = document.createElement('div')
-//         task_actions_el.classList.add('actions')
-
-//         const task_done_el = document.createElement('button')
-//         task_done_el.classList.add('done')
-//         const done_button = document.createElement('span')
-//         done_button.classList.add('material-symbols-outlined')
-//         done_button.textContent = 'check_circle'
-
-
-//         const task_edit_el = document.createElement('button')
-//         task_edit_el.classList.add('edit')
-//         task_edit_el.innerHTML = 'Edit'
+        // ------------------- CREATE/READ -------------------
+        const task_el = document.createElement('div')
+        task_el.classList.add('task')
         
-//         const task_delete_el = document.createElement('button')
-//         task_delete_el.classList.add('delete')
-//         const delete_button = document.createElement('span')
-//         delete_button.classList.add('material-symbols-outlined')
-//         delete_button.textContent = 'delete'
 
-//         task_actions_el.appendChild(task_done_el)
-//         task_actions_el.appendChild(task_edit_el)
-//         task_actions_el.appendChild(task_delete_el)
+        const task_content_el = document.createElement('div')
+        task_content_el.classList.add('content')
 
-//         task_done_el.appendChild(done_button)
-//         task_delete_el.appendChild(delete_button)
+        task_el.appendChild(task_content_el)
+
+        const task_input_el = document.createElement('input')
+        task_input_el.classList.add('text')
+        task_input_el.type = 'text'
+        task_input_el.value = task
+        task_input_el.setAttribute('readonly', 'readonly')
+        task_input_el.setAttribute('id', idGenerator())
+
+        task_content_el.appendChild(task_input_el)
+
+        const task_actions_el = document.createElement('div')
+        task_actions_el.classList.add('actions')
+
+        const task_done_el = document.createElement('button')
+        task_done_el.classList.add('done')
+        const done_button = document.createElement('span')
+        done_button.classList.add('material-symbols-outlined')
+        done_button.textContent = 'check_circle'
 
 
-//         task_el.appendChild(task_actions_el)
+        const task_edit_el = document.createElement('button')
+        task_edit_el.classList.add('edit')
+        task_edit_el.innerHTML = 'Edit'
+        
+        const task_delete_el = document.createElement('button')
+        task_delete_el.classList.add('delete')
+        const delete_button = document.createElement('span')
+        delete_button.classList.add('material-symbols-outlined')
+        delete_button.textContent = 'delete'
 
-//         list_el.appendChild(task_el)
+        task_actions_el.appendChild(task_done_el)
+        task_actions_el.appendChild(task_edit_el)
+        task_actions_el.appendChild(task_delete_el)
 
-//         input.value = ''
+        task_done_el.appendChild(done_button)
+        task_delete_el.appendChild(delete_button)
 
 
-//         // ------------------- UPDATE -------------------
-//         task_edit_el.addEventListener('click', () => {
-//             if (task_edit_el.innerHTML.toLowerCase() === 'edit') {
-//                 task_input_el.removeAttribute('readonly')
-//                 task_input_el.focus()
-//                 task_edit_el.innerHTML = 'Save'
-//             } else {
-//                 task_input_el.setAttribute('readonly', 'readonly')
-//                 task_edit_el.innerHTML = 'Edit'
-//             }
-//         })
+        task_el.appendChild(task_actions_el)
 
-//         // ------------------- DONE -------------------
+        list_el.appendChild(task_el)
 
-//         done_button.addEventListener('click', () => {
-//             if (task_content_el.className === 'content') {
-//                 task_content_el.classList.add('-done')
-//                 task_actions_el.removeChild(task_edit_el)
+        input.value = ''
+
+
+        // ------------------- UPDATE -------------------
+        task_edit_el.addEventListener('click', () => {
+            if (task_edit_el.innerHTML.toLowerCase() === 'edit') {
+                task_input_el.removeAttribute('readonly')
+                task_input_el.focus()
+                task_edit_el.innerHTML = 'Save'
+            } else {
+                task_input_el.setAttribute('readonly', 'readonly')
+                task_edit_el.innerHTML = 'Edit'
+            }
+        })
+
+        // ------------------- DONE -------------------
+
+        done_button.addEventListener('click', () => {
+            if (task_content_el.className === 'content') {
+                task_content_el.classList.add('-done')
+                task_actions_el.removeChild(task_edit_el)
                 
-//             } else {
-//                 task_content_el.classList.remove('-done')
-//                 task_actions_el.appendChild(task_edit_el)
-//                 task_actions_el.appendChild(task_delete_el)
-//             }        
-//         })
+            } else {
+                task_content_el.classList.remove('-done')
+                task_actions_el.appendChild(task_edit_el)
+                task_actions_el.appendChild(task_delete_el)
+            }        
+        })
 
 
-//         // ------------------- DELETE -------------------
-//         task_delete_el.addEventListener('click', () => {
-//             list_el.removeChild(task_el)
-//         })
-//     })
-// })
+        // ------------------- DELETE -------------------
+        task_delete_el.addEventListener('click', () => {
+            list_el.removeChild(task_el)
+        })
+    })
+})
--- src/js/taskStorage.js
+++ src/js/taskStorage.js
@@ -6,58 +6,58 @@ function idGenerator() {
 }
 // ---------------- Fake/teste banco de dados do LocalStorage ----------------
 
-let bdLocalStorage = [
-    {'task_txt': 'Estudar JS', 'status': ''},
-    {'task_txt': 'Estudar CSS', 'status': '-done'}
-]
+// let bdLocalStorage = [
+//     {'task_txt': 'Estudar JS', 'status': ''},
+//     {'task_txt': 'Estudar CSS', 'status': '-done'}
+// ]
 
 
 // -------------------------------- CRIAR TASK --------------------------------
 
-const criarTask = (taskTxt, status) => {
-    const task = document.createElement('div');
-    task.classList.add('task')
-    task.innerHTML = `
-        <div class="content ${status}">
-        <input
-            id=${idGenerator()}
-            type="text"
-            class="text"
-            value="${taskTxt}"
-            readonly />
-        </div>
-        <div class="actions">
-            <button class="done">
-                <span class="material-symbols-outlined">
-                    check_circle
-                </span>
-            </button>
-            <button class="edit">
-                Edit
-            </button>
-            <button class="delete">
-                <span class="material-symbols-outlined">
-                    delete
-                </span>
-            </button>
-    `
-    document.getElementById('tasks').appendChild(task)
-}
+// const criarTask = (taskTxt, status) => {
+//     const task = document.createElement('div');
+//     task.classList.add('task')
+//     task.innerHTML = `
+//         <div class="content ${status}">
+//         <input
+//             id=${idGenerator()}
+//             type="text"
+//             class="text"
+//             value="${taskTxt}"
+//             readonly />
+//         </div>
+//         <div class="actions">
+//             <button class="done">
+//                 <span class="material-symbols-outlined">
+//                     check_circle
+//                 </span>
+//             </button>
+//             <button class="edit">
+//                 Edit
+//             </button>
+//             <button class="delete">
+//                 <span class="material-symbols-outlined">
+//                     delete
+//                 </span>
+//             </button>
+//     `
+//     document.getElementById('tasks').appendChild(task)
+// }
 
 
 // ---------------------- Update localStorage list ----------------------
-const  tasksClear = () => {
-    const tasksList = document.getElementById('tasks')
-    while (tasksList.firstChild) {
-        tasksList.removeChild(tasksList.lastChild)
-    }
-}
+// const  tasksClear = () => {
+//     const tasksList = document.getElementById('tasks')
+//     while (tasksList.firstChild) {
+//         tasksList.removeChild(tasksList.lastChild)
+//     }
+// }
 
 
 // ------------------------ Update Screen list ------------------------
-const updateScreen = () => {
-    tasksClear()
-    bdLocalStorage.forEach (task => criarTask (task.task_txt, task.status, task.id))
-}
+// const updateScreen = () => {
+//     tasksClear()
+//     bdLocalStorage.forEach (task => criarTask (task.task_txt, task.status, task.id))
+// }
 
-updateScreen(console.log(bdLocalStorage))
+// updateScreen(console.log(bdLocalStorage))
